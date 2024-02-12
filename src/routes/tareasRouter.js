ad84df12529e75cb6e7f84cb24c9c5254c2a5557
jcentenaro <info@pitzil.com>
2024-02-12 17:58:01 -0300

    create tasks

--- src/routes/tareasRouter.js
+++ src/routes/tareasRouter.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const controller = require("../controllers/tareasControllers");
 
 router.get("/", controller.index);
+router.post("/", controller.store);
 
 module.exports = router;
